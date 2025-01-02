0f323c9e46aa012eeda12703c62ec42ca2fa4530
VANIZA  KHAN <VK120@users.noreply.huggingface.co>
2025-01-02 08:45:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+pip install streamlit groq
 import os
 import streamlit as st
 from groq import Groq
 
 # Set your Groq API key
-os.environ["GROQ_API_KEY"] = "your_api_key_here"
+os.environ["GROQ_API_KEY"] = "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"
 
 # Initialize the Groq client
 client = Groq(
